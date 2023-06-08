7fbb7bddec37d7e0c732910d224320840ded5cd6
Shaw Lu <shawlu95@gmail.com>
2023-06-08 00:10:00 -0700

    simplify play audio on key press

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -24,20 +24,9 @@ class ViewController: UIViewController {
     }
 
     func playSound() {
-        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }
-        do {
-            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-            try AVAudioSession.sharedInstance().setActive(true)
-
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
-            
-            guard let player = player else { return }
-            
-            player.play()
-        } catch let error {
-            print(error.localizedDescription)
-        }
-        
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player!.play()
     }
 }
 
