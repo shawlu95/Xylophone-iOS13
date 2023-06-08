56e3c1ce280d04e2eb6eac1bbc994e71a9e7bc41
Shaw Lu <shawlu95@gmail.com>
2023-06-08 00:27:35 -0700

    dim button on pressed

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -22,6 +22,15 @@ class ViewController: UIViewController {
         let key = sender.currentTitle!
         print("button pressed \(key)")
         playSound(key: key)
+        
+        //Reduces the sender's (the button that got pressed) opacity to half.
+        sender.alpha = 0.5
+                
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
     }
 
     func playSound(key: String = "C") {
