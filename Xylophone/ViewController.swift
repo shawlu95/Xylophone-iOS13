d86ed9a8fef5404e459021074db3d45594711e0e
Shaw Lu <shawlu95@gmail.com>
2023-06-08 00:06:51 -0700

    play audio on key press

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,16 +7,37 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    // play C sound when button is pressed
     @IBAction func keyPressed(_ sender: UIButton) {
         print("button pressed")
+        playSound()
     }
 
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            
+            guard let player = player else { return }
+            
+            player.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+    }
 }
 
